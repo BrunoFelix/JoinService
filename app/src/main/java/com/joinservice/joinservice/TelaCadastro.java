454b287e95f3b16576af416c7694d609e3925563
tjborges <tj.batera1@gmail.com>
2017-09-12 16:07:11 -0300

    Add Logo e Nome na Tela de Login

--- app/src/main/java/com/joinservice/joinservice/TelaCadastro.java
+++ app/src/main/java/com/joinservice/joinservice/TelaCadastro.java
@@ -2,6 +2,11 @@ package com.joinservice.joinservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TelaCadastro extends AppCompatActivity {
 
@@ -11,3 +16,5 @@ public class TelaCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_tela_cadastro);
     }
 }
+
+
