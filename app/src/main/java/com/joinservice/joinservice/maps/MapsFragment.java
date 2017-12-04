04e0bc72bc33298259461aa8eac55231eda8f9cf
BrunoFelix <brunofelixbarbosa123@hotmail.com>
2017-12-04 00:51:26 -0300

    AsynkTask para traçar a rota no googlemaps.

--- app/src/main/java/com/joinservice/joinservice/maps/MapsFragment.java
+++ app/src/main/java/com/joinservice/joinservice/maps/MapsFragment.java
@@ -3,43 +3,32 @@ package com.joinservice.joinservice.maps;
 import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.AsyncTask;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
-import com.joinservice.joinservice.R;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MapsFragment extends SupportMapFragment implements OnMapReadyCallback  {
 
@@ -49,14 +38,10 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     boolean exibirRota;
     double longitudeProfissional;
     double latitudeProfissional;
-
-    private List<Marker> originMarkers = new ArrayList<>();
-    private List<Marker> destinationMarkers = new ArrayList<>();
-    private List<Polyline> polylinePaths = new ArrayList<>();
-    private ProgressDialog progressDialog;
-    private long distance;
     private Polyline polyline;
     private List<LatLng> list;
+
+
 
     public MapsFragment() {
     }
@@ -77,6 +62,8 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
             }
         }
 
+        Route Route = new Route(new LatLng(latitude, longitude), new LatLng(latitudeProfissional, longitudeProfissional));
+        Route.execute();
 
     }
 
@@ -103,8 +90,8 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
 
 
         if (exibirRota) {
-            LatLngBounds zoom = new LatLngBounds(
-                    new LatLng(latitude, longitude), new LatLng(latitudeProfissional, longitudeProfissional));
+            /*LatLngBounds zoom = new LatLngBounds(
+                    new LatLng(latitude, longitude), new LatLng(latitudeProfissional, longitudeProfissional));*/
 
             //Defini um marcador no local onde o serviço deve ser executado
             LatLng marcacao = new LatLng(latitude, longitude);
@@ -118,9 +105,12 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
             mo.position(marcacao).title("Serviço deve ser feito aqui");
             mMap.addMarker(mo);
 
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoom.getCenter(), 10));
 
-            getRoute(new LatLng(latitude, longitude), new LatLng(latitudeProfissional, longitudeProfissional));
+            LatLng marcador = new LatLng(latitude,longitude);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marcador, 15));
+
+          // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zoom.getCenter(), 10));
+
         }else{
             LatLng marcador = new LatLng(latitude,longitude);
 
@@ -134,65 +124,201 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         }
     }
 
-    /* ***************************************** ROTA ***************************************** */
+    public class Route extends AsyncTask<Void, Void, List<LatLng>> {
+        private List<Marker> originMarkers = new ArrayList<>();
+        private List<Marker> destinationMarkers = new ArrayList<>();
+        private List<Polyline> polylinePaths = new ArrayList<>();
+        private ProgressDialog progressDialog;
+        private long distance;
+        private List<LatLng> listaRouter;
+        ProgressDialog load;
 
+        LatLng origin;
+        LatLng destination;
 
+        public Route(LatLng origin, LatLng destination){
+            this.origin = origin;
+            this.destination = destination;
+        }
 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            load = new ProgressDialog(getActivity());
+            // Setting Title
+            load.setTitle("Traçando rota, aguarde...");
+            // Setting Message
+            load.setMessage("encontrando a melhor rota entre você e o serviço...");
+            // Progress Dialog Style Horizontal
+            load.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            /*// Progress Dialog Style Spinner
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);*/
+            load.setMax(100);
+            load.show();
+        }
 
-    // WEB CONNECTION
-    //public void getRoute(final String origin, final String destination){
-    public void getRoute(final LatLng origin, final LatLng destination)  {
-        new Thread(){
-            public void run(){
-						/*String url= "http://maps.googleapis.com/maps/api/directions/json?origin="
-								+ origin+"&destination="
-								+ destination+"&sensor=false";*/
-                String link= "http://maps.googleapis.com/maps/api/directions/json?origin="
-                        + origin.latitude+","+origin.longitude+"&destination="
-                        + destination.latitude+","+destination.longitude+"&sensor=false";
+        @Override
+        protected List<LatLng> doInBackground(Void... voids) {
+            load.incrementProgressBy(10);
+            listaRouter = getRoute();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            load.incrementProgressBy(10);
+            return listaRouter;
+        }
 
-                URL url = null;
-                try {
-                    url = new URL(link);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
+        @Override
+        protected void onPostExecute(List<LatLng> latLngs) {
+            list = listaRouter;
+            load.incrementProgressBy(10);
+            load.dismiss();
+            tracarRota();
 
-                InputStream is = null;
-                try {
-                    is = url.openConnection().getInputStream();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                final StringBuffer buffer = new StringBuffer();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        }
 
-                String line;
-                try {
-                    while ((line = reader.readLine()) != null) {
-                        buffer.append(line + "\n");
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        public void tracarRota(){
+            if (list != null) {
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        //Log.i("Script", answer);
+                        drawRoute();
 
-
-                getActivity().runOnUiThread(new Runnable(){
-                    public void run(){
-                        try {
-                            //Log.i("Script", answer);
-                            list = buildJSONRoute(buffer.toString());
-                            if (!list.isEmpty()) {
-                                drawRoute();
-                            }
-                        }
-                        catch(JSONException e) {
-                            e.printStackTrace();
-                        }
                     }
                 });
             }
-        }.start();
+        }
+
+        // WEB CONNECTION
+        //public void getRoute(final String origin, final String destination){
+        public List<LatLng> getRoute()  {
+            String link= "http://maps.googleapis.com/maps/api/directions/json?origin="
+                    + origin.latitude+","+origin.longitude+"&destination="
+                    + destination.latitude+","+destination.longitude+"&sensor=false";
+
+            URL url = null;
+            try {
+                url = new URL(link);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            load.incrementProgressBy(10);
+
+            InputStream is = null;
+            try {
+                is = url.openConnection().getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            load.incrementProgressBy(10);
+
+            final StringBuffer buffer = new StringBuffer();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            load.incrementProgressBy(10);
+
+            try {
+                list = buildJSONRoute(buffer.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            load.incrementProgressBy(10);
+
+            return list;
+        /*this.runOnUiThread(new Runnable(){
+            public void run(){
+                try {
+                    //Log.i("Script", answer);
+                    list = buildJSONRoute(buffer.toString());
+                    if (!list.isEmpty()) {
+                        drawRoute();
+                    }
+                }
+                catch(JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
+
+        }
+
+        // PARSER JSON
+        public List<LatLng> buildJSONRoute(String json) throws JSONException{
+            JSONObject result = new JSONObject(json);
+            JSONArray routes = result.getJSONArray("routes");
+
+            distance = routes.getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONObject("distance").getInt("value");
+
+            load.incrementProgressBy(10);
+
+            JSONArray steps = routes.getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONArray("steps");
+            List<LatLng> lines = new ArrayList<LatLng>();
+
+            for(int i=0; i < steps.length(); i++) {
+                Log.i("Script", "STEP: LAT: "+steps.getJSONObject(i).getJSONObject("start_location").getDouble("lat")+" | LNG: "+steps.getJSONObject(i).getJSONObject("start_location").getDouble("lng"));
+
+
+                String polyline = steps.getJSONObject(i).getJSONObject("polyline").getString("points");
+
+                for(LatLng p : decodePolyline(polyline)) {
+                    lines.add(p);
+                }
+
+                Log.i("Script", "STEP: LAT: "+steps.getJSONObject(i).getJSONObject("end_location").getDouble("lat")+" | LNG: "+steps.getJSONObject(i).getJSONObject("end_location").getDouble("lng"));
+            }
+
+            load.incrementProgressBy(10);
+
+            return(lines);
+        }
+
+        // DECODE POLYLINE
+        private List<LatLng> decodePolyline(String encoded) {
+
+            List<LatLng> listPoints = new ArrayList<LatLng>();
+            int index = 0, len = encoded.length();
+            int lat = 0, lng = 0;
+
+            while (index < len) {
+                int b, shift = 0, result = 0;
+                do {
+                    b = encoded.charAt(index++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lat += dlat;
+
+                shift = 0;
+                result = 0;
+                do {
+                    b = encoded.charAt(index++) - 63;
+                    result |= (b & 0x1f) << shift;
+                    shift += 5;
+                } while (b >= 0x20);
+                int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+                lng += dlng;
+
+                LatLng p = new LatLng((((double) lat / 1E5)), (((double) lng / 1E5)));
+                Log.i("Script", "POL: LAT: "+p.latitude+" | LNG: "+p.longitude);
+                listPoints.add(p);
+            }
+            return listPoints;
+        }
     }
 
     public void drawRoute(){
@@ -212,65 +338,5 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         else{
             polyline.setPoints(list);
         }
-    }
-
-    // PARSER JSON
-    public List<LatLng> buildJSONRoute(String json) throws JSONException{
-        JSONObject result = new JSONObject(json);
-        JSONArray routes = result.getJSONArray("routes");
-
-        distance = routes.getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONObject("distance").getInt("value");
-
-        JSONArray steps = routes.getJSONObject(0).getJSONArray("legs").getJSONObject(0).getJSONArray("steps");
-        List<LatLng> lines = new ArrayList<LatLng>();
-
-        for(int i=0; i < steps.length(); i++) {
-            Log.i("Script", "STEP: LAT: "+steps.getJSONObject(i).getJSONObject("start_location").getDouble("lat")+" | LNG: "+steps.getJSONObject(i).getJSONObject("start_location").getDouble("lng"));
-
-
-            String polyline = steps.getJSONObject(i).getJSONObject("polyline").getString("points");
-
-            for(LatLng p : decodePolyline(polyline)) {
-                lines.add(p);
-            }
-
-            Log.i("Script", "STEP: LAT: "+steps.getJSONObject(i).getJSONObject("end_location").getDouble("lat")+" | LNG: "+steps.getJSONObject(i).getJSONObject("end_location").getDouble("lng"));
-        }
-
-        return(lines);
-    }
-
-    // DECODE POLYLINE
-    private List<LatLng> decodePolyline(String encoded) {
-
-        List<LatLng> listPoints = new ArrayList<LatLng>();
-        int index = 0, len = encoded.length();
-        int lat = 0, lng = 0;
-
-        while (index < len) {
-            int b, shift = 0, result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lat += dlat;
-
-            shift = 0;
-            result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lng += dlng;
-
-            LatLng p = new LatLng((((double) lat / 1E5)), (((double) lng / 1E5)));
-            Log.i("Script", "POL: LAT: "+p.latitude+" | LNG: "+p.longitude);
-            listPoints.add(p);
-        }
-        return listPoints;
     }
 }
